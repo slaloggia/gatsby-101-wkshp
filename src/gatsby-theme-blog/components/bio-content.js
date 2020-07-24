978b915d3bba97621d34096b4508f030d7f4c0e8
slaloggia <sarah.laloggia@gmail.com>
2020-07-24 17:50:21 -0500

    Add avatar image and bio-content component

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+  <Fragment>
+    Words by <Styled.a href="http://sarahlaloggia.com/">Sarah Laloggia</Styled.a>.
+    <br />
+    I'm tired.
+  </Fragment>
+)
+
+export default BioContent
